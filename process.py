1fd45d50a09b33fcb8e50904e2de76ac35d1fff7
Joabljr <joabljr97@gmail.com>
2022-02-20 01:10:15 -0500

    Assesment_5

---
+++ process.py
@@ -0,0 +1,13 @@
+log_file = open("um-server-01.txt")
+
+
+def sales_reports(log_file):
+    for line in log_file:
+        line = line.rstrip()
+        day = line[0:3]
+        if day == "Tue":
+            print(line)
+
+
+# line 1 is linking the code to the server
+# line 4 is 
